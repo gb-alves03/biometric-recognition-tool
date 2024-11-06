bad8bfc962b566e12720840dc72b11096ca7ed8d
Giovanni Marques <giovanni.marques030@gmail.com>
2024-11-06 11:29:10 -0300

    feat: create account entity

---
+++ src/main/kotlin/com/br/biometric_tool/domain/entity/Account.kt
@@ -0,0 +1,26 @@
+package com.br.biometric_tool.domain.entity
+
+import java.util.UUID
+
+class Account(
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    biometricsEnabled: Boolean,
+    biometricsUrl: String = null.toString()
+) {
+    val accountId: String
+    private var name: Name
+    private var email: Email
+    private var password: Password
+    private var biometricsEnabled: Boolean = false
+    var biometricsUrl: String? = null
+
+    init {
+        this.accountId = UUID.randomUUID().toString()
+        this.name = Name(firstName, lastName)
+        this.email = Email(email)
+        this.password = Password.create(password)
+    }
+}
