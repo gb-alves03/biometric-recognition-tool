c1d838a1436dc0ad9bc7fada2fbc486ea554cd1b
Giovanni Marques <giovanni.marques030@gmail.com>
2024-11-06 11:46:48 -0300

    fix: change in vo name construction

--- src/main/kotlin/com/br/biometric_tool/domain/vo/Name.kt
+++ src/main/kotlin/com/br/biometric_tool/domain/vo/Name.kt
@@ -5,14 +5,20 @@ class Name(firstName: String, lastName: String) {
     private var lastName: String
 
     init {
-        val regex = Regex("^[A-Za-z]{2,50}$")
-        if(!regex.matches(firstName)) throw IllegalArgumentException("O primeiro nome deve conter apenas letras e ter entre 2 e 50 caracteres.")
-        if(!regex.matches(lastName)) throw IllegalArgumentException("O ultimo nome deve conter apenas letras e ter entre 2 e 50 caracteres.")
+        if(!firstName.matches(Regex("^[A-Za-z]{2,50}$"))) throw IllegalArgumentException("Invalid first name.")
+        if(!lastName.matches(Regex("^[A-Za-z]{2,50}$"))) throw IllegalArgumentException("Invalid last name.")
         this.firstName = firstName
         this.lastName = lastName
     }
 
-    fun getFirstName(): String = this.firstName
-    fun getLastName(): String = this.lastName
-    fun getName(): String  = "$firstName $lastName"
+    fun getFirstName(): String {
+        return this.firstName
+    }
+
+    fun getLastName(): String {
+        return this.lastName
+    }
+    fun getName(): String {
+        return "$firstName $lastName"
+    }
 }
