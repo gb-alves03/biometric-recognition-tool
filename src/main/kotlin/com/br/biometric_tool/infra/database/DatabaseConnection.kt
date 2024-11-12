3e6e08a846e263ab8fb88661af8a0b23085da510
Gabriel Alves <gabrielhenriquealves321@gmail.com>
2024-11-12 01:41:46 -0300

    delete: delete unusable classes

--- src/main/kotlin/com/br/biometric_tool/infra/database/DatabaseConnection.kt
+++
@@ -1,8 +0,0 @@
-package com.br.biometric_tool.infra.database
-
-import com.br.biometric_tool.infra.exceptions.DatabaseNotConnectedException
-import java.io.FileInputStream
-import java.sql.Connection
-import java.sql.DriverManager
-import java.util.Properties
-
--- src/main/kotlin/com/br/biometric_tool/infra/database/DatabaseManipulation.kt
+++
@@ -1,8 +0,0 @@
-package com.br.biometric_tool.infra.database
-
-import com.br.biometric_tool.infra.exceptions.SchemaExecutionFailedException
-import com.br.biometric_tool.infra.exceptions.SchemaFileNotFoundException
-import java.io.File
-import java.sql.Connection
-import java.sql.Statement
-
