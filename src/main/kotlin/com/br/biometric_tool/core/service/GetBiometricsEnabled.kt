4a999229648e376a843eb32cde17d770b4bf3572
Giovanni Marques <giovanni.marques030@gmail.com>
2024-11-11 23:21:02 -0300

    feat/add-not-found-exception

--- src/main/kotlin/com/br/biometric_tool/core/service/GetBiometricsEnabled.kt
+++ src/main/kotlin/com/br/biometric_tool/core/service/GetBiometricsEnabled.kt
@@ -3,10 +3,11 @@ package com.br.biometric_tool.core.service
 import com.br.biometric_tool.core.dto.GetBiometricsEnabledInput
 import com.br.biometric_tool.core.dto.GetBiometricsEnabledOutput
 import com.br.biometric_tool.core.repository.AccountRepository
+import com.br.biometric_tool.infra.exceptions.NotFoundException
 
 class GetBiometricsEnabled(private val accountRepository: AccountRepository) {
     fun execute(input: GetBiometricsEnabledInput): GetBiometricsEnabledOutput {
-        val account = accountRepository.findByEmail(input.email) ?: throw IllegalArgumentException("Account not found for email: ${input.email}")
+        val account = accountRepository.findByEmail(input.email) ?: throw NotFoundException("Account not found for email: ${input.email}")
         return GetBiometricsEnabledOutput(account.isBiometricsEnabled())
     }
 }
