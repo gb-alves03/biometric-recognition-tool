8ab48db3631bb217e590a2f52d68006890c6e078
Gabriel Alves <gabrielhalves03@gmail.com>
2024-11-12 02:32:01 -0300

    refactor: refactor InvalidStatus exception

--- src/main/kotlin/com/br/biometric_tool/core/domain/vo/Name.kt
+++ src/main/kotlin/com/br/biometric_tool/core/domain/vo/Name.kt
@@ -1,14 +1,14 @@
 package com.br.biometric_tool.core.domain.vo
 
-import com.br.biometric_tool.infra.exceptions.InvalidStatus
+import com.br.biometric_tool.infra.exceptions.InvalidStatusException
 
 class Name(firstName: String, lastName: String) {
     private var firstName: String
     private var lastName: String
 
     init {
-        if(!firstName.matches(Regex("^[A-Za-z]{2,50}$"))) throw InvalidStatus("Invalid first name.")
-        if(!lastName.matches(Regex("^[A-Za-z]{2,50}$"))) throw InvalidStatus("Invalid last name.")
+        if(!firstName.matches(Regex("^[A-Za-z]{2,50}$"))) throw InvalidStatusException("Invalid first name.")
+        if(!lastName.matches(Regex("^[A-Za-z]{2,50}$"))) throw InvalidStatusException("Invalid last name.")
         this.firstName = firstName
         this.lastName = lastName
     }
--- src/main/kotlin/com/br/biometric_tool/infra/exceptions/InvalidStatusException.kt
+++ src/main/kotlin/com/br/biometric_tool/infra/exceptions/InvalidStatusException.kt
@@ -1,6 +1,6 @@
 package com.br.biometric_tool.infra.exceptions
 
-class InvalidStatus : RuntimeException {
+class InvalidStatusException : RuntimeException {
     private val status: Int = 400
 
     constructor(message: String) : super(message)
